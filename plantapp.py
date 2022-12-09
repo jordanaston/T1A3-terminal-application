c2c2e5e4881b14a73a0023f84672a6b3b99e06f7
Jordan Aston <gcab022124@coderacademy.edu.au>
2022-12-09 21:02:07 +1000

    function created for adding/ removing plant from user collection

--- plantapp.py
+++ plantapp.py
@@ -3,6 +3,12 @@ from prettytable import PrettyTable
 from plant_class import Plant
 
 your_table = PrettyTable(['Plant Name','No. Days Since Watered', 'No. Days Since Re-Pot','Near Window'])
+your_table.add_row(["MONSTERA",'','','',])
+your_table.add_row(["DEVIL'S IVY",'','','',])
+your_table.add_row(["PEACE LILY",'','','',])
+your_table.add_row(["FICUS",'','','',])
+your_table.add_row(["SUCCULENT",'','','',])
+
 
 MONSTERA = Plant("monstera", 11, 365, True)
 DEVILS_IVY = Plant("devils ivy", 7, 365, False)
@@ -10,16 +16,30 @@ PEACE_LILY = Plant("peace lily", 7, 730, False)
 FICUS = Plant("ficus", 5, 730, True)
 SUCCULENT = Plant("succulent", 3, 730, True)
 
-# todays_date = ""
+print("\nWelcome to PLANTAPP. \n")
+print("This app is designed to assist you with some basic care taking of your indoor house-plants. \n")
+print("Currently, this app supports the following popular indoor house-plants: \n\nMONSTERA\nDEVIL'S IVY\nPEACE LILY\nFICUS\nSUCCULENT\n")
+print("If you have any plants outside of this list, please come back another time when our list is updated! \nBut for now, let's help you out with what we support :) \n")
+print("To get started, tell me a little bit about your current house-plant situation. \n")
 
-print("Welcome to PLANTAPP.")
+
+# ---------------------------
 
 instructions = "\n1: Enter 'A' to ADD a new plant to your collection. \n2: Enter 'D' to DELETE a plant from your collection. "
 
 print(instructions)
 
-print("\nWhat would you like to do?\n")
+# ---------------------------
 
-# print(str(input(your_table.add_row())))
+add_delete_plant = input("\nWhat would you like to do?\n")
 
-print(your_table)
+def update_plant_collection():
+    if add_delete_plant == "A":
+        print("You chose to add a plant!")
+    else:
+        print("You didn't select anything...")
+
+update_plant_collection()
+
+# print(your_table)
+
