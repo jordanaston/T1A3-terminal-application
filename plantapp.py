444bc131f5790c3b91914025b5eb17fe208e66e6
Jordan Aston <gcab022124@coderacademy.edu.au>
2022-12-10 16:15:54 +1000

    feature two function started

--- plantapp.py
+++ plantapp.py
@@ -17,15 +17,16 @@ FICUS = Plant("ficus", 5, 730, True)
 SUCCULENT = Plant("succulent", 3, 730, True)
 
 # Introduction to the app
-print("\nWelcome to PLANTAPP. \n")
+print("\n\n\n\nWelcome to PLANTAPP. \n")
 print("This app is designed to assist you with some basic care taking of your indoor house-plants. \n")
 print("Currently, this app supports the following popular indoor house-plants: \n\nMONSTERA\nPOTHOS\nPEACE LILY\nFICUS\nSUCCULENT\n")
 print("If you have any plants outside of this list, please come back another time when our list is updated! \nBut for now, let's help you out with what we support :) \n")
-print("To get started, tell me a little bit about your current house-plant situation. \n")
+input("\n\nPress ENTER to continue...\n\n")
+print("To get started, press 'A' to add your first plant to your collection. \n")
 
 # Instructions for user input
 instructions = "\nINSTRUCTIONS: \n1: Enter 'A' to ADD a new plant to your collection. \n2: Enter 'R' to REMOVE a plant from your collection. \n3. Enter 'F' to FINALISE your plant collection. \n "
-print(instructions)
+# print(instructions)
 
 # Function for adding and removing plants to the users plant collection.
 def update_plant_collection():
@@ -73,37 +74,37 @@ def update_plant_collection():
             update_plant_collection()
 
     elif user_option.upper() == "R":
-        remove_plant = input("Which plant would you like to remove? \n'1' for ROW 1\n'2' for ROW 2\n'3' for ROW 3 and so on...\n ")
+        remove_plant = input("\nWhich plant would you like to remove? \n'1' for ROW 1\n'2' for ROW 2\n'3' for ROW 3 and so on...\n")
         if remove_plant == "1":
-            print("\n You removed plant 1 from your collection. \n")
+            print("\n You removed PLANT 1 from your collection. \n")
             your_table.del_row(0)
             print(your_table)
             print(instructions)
             update_plant_collection()
 
         elif remove_plant == "2":
-            print("\n You removed plant 2 from your collection. \n")
+            print("\n You removed PLANT 2 from your collection. \n")
             your_table.del_row(1)
             print(your_table)
             print(instructions)
             update_plant_collection()
 
         elif remove_plant == "3":
-            print("\n You removed plant 3 from your collection. \n")
+            print("\n You removed PLANT 3 from your collection. \n")
             your_table.del_row(2)
             print(your_table)
             print(instructions)
             update_plant_collection()
 
         elif remove_plant == "4":
-            print("\n You removed plant 4 from your collection. \n")
+            print("\n You removed PLANT 4 from your collection. \n")
             your_table.del_row(3)
             print(your_table)
             print(instructions)
             update_plant_collection()
 
         elif remove_plant == "5":
-            print("\n You removed plant 5 from your collection. \n")
+            print("\n You removed PLANT 5 from your collection. \n")
             your_table.del_row(4)
             print(your_table)
             print(instructions)
@@ -125,3 +126,20 @@ update_plant_collection()
 print(your_table)
 print("\nThe above table is your final collection.\n")
 
+input("\n\nPress ENTER to continue...\n\n")
+
+print("\nTell me a little bit more about your " + (your_table.rows[0][0]) + "...\n")
+
+def fill_table_data():
+    row_one_data = []
+    if your_table.rows[0][0] == "MONSTERA" or "POTHOS" or "PEACE LILY" or "FICUS" or "SUCCULENT":
+        add_plant_data = input("How many days since you last watered " + (your_table.rows[0][0]) + "? ")
+        row_one_data.append(add_plant_data)
+        add_plant_data = (input("How many days since you last re-potted " + (your_table.rows[0][0]) + "? "))
+        row_one_data.append(add_plant_data)
+        add_plant_data = bool((input("Yes or No, do you keep your " + (your_table.rows[0][0]) + " near a window? ")))
+        row_one_data.append(add_plant_data)
+        
+        print(row_one_data)
+        
+fill_table_data()
