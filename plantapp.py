795bf686af13ae6eaa358c6a6069be624d18d479
Jordan Aston <gcab022124@coderacademy.edu.au>
2022-12-13 22:53:33 +1000

    refactoring code for feature1 and 2

--- plantapp.py
+++ plantapp.py
@@ -21,21 +21,21 @@ program_plant_name_list = []
 program_plant_data_list = []
 
 # Object Data for Plant Class
-# MONSTERA = Plant("MONSTERA", 11, 365, True)
-# POTHOS = Plant("POTHOS", 7, 365, False)
-# PEACE_LILY = Plant("PEACE_LILY", 7, 730, False)
-# FICUS = Plant("FICUS", 5, 730, True)
-# SUCCULENT = Plant("SUCCULENT", 3, 730, True)
-# DRACAENA = Plant("DRACAENA", 12, 1460, False)
-# ALOE_VERA = Plant("ALOE_VERA", 18, 1095, True)
-# PEPEROMIA = Plant("PEPEROMIA", 10, 730, True)
-# SNAKE_PLANT = Plant("SNAKE_PLANT", 7, 2555, True)
-# TRADESCANTIA = Plant("TRADESCANTIA", 7, 356, False)
-# CHINESE_EVERGREEN = Plant("CHINESE_EVERGREEN", 8, 730, True)
-# HOYA = Plant("HOYA", 14, 2190, False)
-# ANTHURIUM = Plant("ANTHURIUM", 7, 730, False)
-# PARLOR_PALM = Plant("PARLOR_PALM", 7, 1095, False)
-# PHILODENDRON = Plant("PHILODENDRON", 11, 1095, True)
+MONSTERA = Plant("MONSTERA", 11, 365, True)
+POTHOS = Plant("POTHOS", 7, 365, False)
+PEACE_LILY = Plant("PEACE_LILY", 7, 730, False)
+FICUS = Plant("FICUS", 5, 730, True)
+SUCCULENT = Plant("SUCCULENT", 3, 730, True)
+DRACAENA = Plant("DRACAENA", 12, 1460, False)
+ALOE_VERA = Plant("ALOE_VERA", 18, 1095, True)
+PEPEROMIA = Plant("PEPEROMIA", 10, 730, True)
+SNAKE_PLANT = Plant("SNAKE_PLANT", 7, 2555, True)
+TRADESCANTIA = Plant("TRADESCANTIA", 7, 356, False)
+CHINESE_EVERGREEN = Plant("CHINESE_EVERGREEN", 8, 730, True)
+HOYA = Plant("HOYA", 14, 2190, False)
+ANTHURIUM = Plant("ANTHURIUM", 7, 730, False)
+PARLOR_PALM = Plant("PARLOR_PALM", 7, 1095, False)
+PHILODENDRON = Plant("PHILODENDRON", 11, 1095, True)
 
 
 # Introduction to the app
@@ -57,11 +57,51 @@ def introduction():
     print("If you have any plants outside of this list, please come back another time when our list is updated! \nBut for now, let's help you out with what we support :) \n")
     input("\n\nPress ENTER to continue...\n\n")
     os.system("clear")
-    print("To get started, press 'A' to add a plant to your collection. \n")
+    # print("To get started, press 'A' to add a plant to your collection. \n")
 introduction()
 
 # Instructions for user input
 instructions = "\nINSTRUCTIONS: \n1: Enter 'A' to ADD a new plant to your collection. \n2: Enter 'R' to REMOVE a plant from your collection. \n3. Enter 'F' to FINALISE your plant collection. \n "
+
+supported_plants = [
+    "MONSTERA", 
+    "POTHOS",
+    "PEACE LILY",
+    "FICUS",
+    "SUCCULENT",
+    "DRACAENA",
+    "ALOE VERA",
+    "PEPEROMIA",
+    "SNAKE PLANT",
+    "TRADESCANTIA",
+    "CHINESE EVERGREEN",
+    "HOYA",
+    "ANTHURIUM",
+    "PARLOR PALM",
+    "PHILODENDRON",
+    ]
+
+# Function to initialize first feature of program and adds the first plant to users collection.
+def get_started():
+    user_option = input("To get started, press 'A' to add a plant to your collection.\n\n")
+    os.system("clear")
+    if user_option.upper() == "A":
+        add_plant = input("\nWhich plant would you like to add?\n\n MONSTERA \n POTHOS \n PEACE LILY \n FICUS \n SUCCULENT \n DRACAENA \n ALOE VERA \n PEPEROMIA \n SNAKE PLANT \n TRADESCANTIA \n CHINESE EVERGREEN \n HOYA \n ANTHURIUM \n PARLOR PALM \n PHILODENDRON \n\n")
+        os.system("clear")
+        if add_plant.upper() in supported_plants:
+            print("\n You added " + add_plant.upper() + "! \n")
+            your_table.add_row([add_plant.upper()])
+            program_plant_name_list.append(add_plant.upper())
+            print(your_table)
+            print(instructions)
+        else:
+            print("IVALID SELECTION!")
+            get_started()        
+    else:
+        get_started()
+get_started()            
+
+      
 
 # Function for adding and removing plants to the users plant collection.
 def update_plant_collection():
@@ -70,266 +110,41 @@ def update_plant_collection():
     if user_option.upper() == "A":
         add_plant = input("\nWhich plant would you like to add?\n\n MONSTERA \n POTHOS \n PEACE LILY \n FICUS \n SUCCULENT \n DRACAENA \n ALOE VERA \n PEPEROMIA \n SNAKE PLANT \n TRADESCANTIA \n CHINESE EVERGREEN \n HOYA \n ANTHURIUM \n PARLOR PALM \n PHILODENDRON \n\n")
         os.system("clear")
-        if add_plant.upper() == "MONSTERA":
-            print("\n You added MONSTERA! \n")
-            your_table.add_row(["MONSTERA"])
-            program_plant_name_list.append("MONSTERA")
+        if add_plant.upper() in supported_plants:
+            print("\n You added " + add_plant.upper() + "! \n")
+            your_table.add_row([add_plant.upper()])
+            program_plant_name_list.append(add_plant.upper())
             print(your_table)
             print(instructions)
             update_plant_collection()
-
-        elif add_plant.upper() == "POTHOS":
-            print("\n You added POTHOS! \n")
-            your_table.add_row(["POTHOS"])
-            program_plant_name_list.append("POTHOS")
-            print(your_table)
-            print(instructions)
-            update_plant_collection()
-
-        elif add_plant.upper() == "PEACE LILY":
-            print("\n You added PEACE LILY! \n")
-            your_table.add_row(["PEACE LILY"])
-            program_plant_name_list.append("PEACE LILY")
-            print(your_table)
-            print(instructions)
-            update_plant_collection()
-
-        elif add_plant.upper() == "FICUS":
-            print("\n You added FICUS! \n")
-            your_table.add_row(["FICUS"])
-            program_plant_name_list.append("FICUS")
-            print(your_table)
-            print(instructions)
-            update_plant_collection()
-
-        elif add_plant.upper() == "SUCCULENT":
-            print("\n You added SUCCULENT! \n")
-            your_table.add_row(["SUCCULENT"])
-            program_plant_name_list.append("SUCCULENT")
-            print(your_table)
-            print(instructions)
-            update_plant_collection()
-        
-        elif add_plant.upper() == "DRACAENA":
-            print("\n You added DRACAENA! \n")
-            your_table.add_row(["DRACAENA"])
-            program_plant_name_list.append("DRACAENA")
-            print(your_table)
-            print(instructions)
-            update_plant_collection()
-        
-        elif add_plant.upper() == "ALOE VERA":
-            print("\n You added ALOE VERA! \n")
-            your_table.add_row(["ALOE VERA"])
-            program_plant_name_list.append("ALOE VERA")
-            print(your_table)
-            print(instructions)
-            update_plant_collection()
-        
-        elif add_plant.upper() == "PEPEROMIA":
-            print("\n You added PEPEROMIA! \n")
-            your_table.add_row(["PEPEROMIA"])
-            program_plant_name_list.append("PEPEROMIA")
-            print(your_table)
-            print(instructions)
-            update_plant_collection()
-
-        elif add_plant.upper() == "SNAKE PLANT":
-            print("\n You added SNAKE PLANT! \n")
-            your_table.add_row(["SNAKE PLANT"])
-            program_plant_name_list.append("SNAKE PLANT")
-            print(your_table)
-            print(instructions)
-            update_plant_collection()
-
-        elif add_plant.upper() == "TRADESCANTIA":
-            print("\n You added TRADESCANTIA! \n")
-            your_table.add_row(["TRADESCANTIA"])
-            program_plant_name_list.append("TRADESCANTIA")
-            print(your_table)
-            print(instructions)
-            update_plant_collection()
-        
-        elif add_plant.upper() == "CHINESE EVERGREEN":
-            print("\n You added CHINESE EVERGREEN! \n")
-            your_table.add_row(["CHINESE EVERGREEN"])
-            program_plant_name_list.append("CHINESE EVERGREEN")
-            print(your_table)
-            print(instructions)
-            update_plant_collection()
-        
-        elif add_plant.upper() == "HOYA":
-            print("\n You added HOYA! \n")
-            your_table.add_row(["HOYA"])
-            program_plant_name_list.append("HOYA")
-            print(your_table)
-            print(instructions)
-            update_plant_collection()
-
-        elif add_plant.upper() == "ANTHURIUM":
-            print("\n You added ANTHURIUM! \n")
-            your_table.add_row(["ANTHURIUM"])
-            program_plant_name_list.append("ANTHURIUM")
-            print(your_table)
-            print(instructions)
-            update_plant_collection()
-
-        elif add_plant.upper() == "PARLOR PALM":
-            print("\n You added PARLOR PALM! \n")
-            your_table.add_row(["PARLOR PALM"])
-            program_plant_name_list.append("PARLOR PALM")
-            print(your_table)
-            print(instructions)
-            update_plant_collection()
-        
-        elif add_plant.upper() == "PHILODENDRON":
-            print("\n You added PHILODENDRON! \n")
-            your_table.add_row(["PHILODENDRON"])
-            program_plant_name_list.append("PHILODENDRON")
-            print(your_table)
-            print(instructions)
-            update_plant_collection()
-        
         else:
             print("\nIVALID SELECTION! Please select 'A' (ADD Plant), 'R' (Remove Plant) or 'F' (Finalise Collection)\n")
             update_plant_collection()
-
+     
     elif user_option.upper() == "R":
         print(your_table)
-        remove_plant = input("\nWhich plant would you like to remove? \n'1' for ROW 1\n'2' for ROW 2\n'3' for ROW 3 and so on...\n")
+        remove_plant = int(input("\nWhich plant would you like to remove? \n\nPress '0' for PLANT 1\nPress '1' for PLANT 2\nPress '2' for PLANT 3 and so on...\n\n"))
         os.system("clear")
-        if remove_plant == "1":
-            print("\n You removed PLANT 1 from your collection. \n")
-            your_table.del_row(0)
-            program_plant_name_list.remove(program_plant_name_list[0])
-            print(your_table)
-            print(instructions)
-            update_plant_collection()
-
-        elif remove_plant == "2":
-            print("\n You removed PLANT 2 from your collection. \n")
-            your_table.del_row(1)
-            program_plant_name_list.remove(program_plant_name_list[1])
-            print(your_table)
-            print(instructions)
-            update_plant_collection()
-
-        elif remove_plant == "3":
-            print("\n You removed PLANT 3 from your collection. \n")
-            your_table.del_row(2)
-            program_plant_name_list.remove(program_plant_name_list[2])
-            print(your_table)
-            print(instructions)
-            update_plant_collection()
-
-        elif remove_plant == "4":
-            print("\n You removed PLANT 4 from your collection. \n")
-            your_table.del_row(3)
-            program_plant_name_list.remove(program_plant_name_list[3])
-            print(your_table)
-            print(instructions)
-            update_plant_collection()
-
-        elif remove_plant == "5":
-            print("\n You removed PLANT 5 from your collection. \n")
-            your_table.del_row(4)
-            program_plant_name_list.remove(program_plant_name_list[4])
-            print(your_table)
-            print(instructions)
-            update_plant_collection()  
-
-        elif remove_plant == "6":
-            print("\n You removed PLANT 6 from your collection. \n")
-            your_table.del_row(5)
-            program_plant_name_list.remove(program_plant_name_list[5])
-            print(your_table)
-            print(instructions)
-            update_plant_collection()  
-
-        elif remove_plant == "7":
-            print("\n You removed PLANT 7 from your collection. \n")
-            your_table.del_row(6)
-            program_plant_name_list.remove(program_plant_name_list[6])
-            print(your_table)
-            print(instructions)
-            update_plant_collection()  
-
-        elif remove_plant == "8":
-            print("\n You removed PLANT 8 from your collection. \n")
-            your_table.del_row(7)
-            program_plant_name_list.remove(program_plant_name_list[7])
-            print(your_table)
-            print(instructions)
-            update_plant_collection()  
-
-        elif remove_plant == "9":
-            print("\n You removed PLANT 9 from your collection. \n")
-            your_table.del_row(8)
-            program_plant_name_list.remove(program_plant_name_list[8])
-            print(your_table)
-            print(instructions)
-            update_plant_collection()  
-
-        elif remove_plant == "10":
-            print("\n You removed PLANT 10 from your collection. \n")
-            your_table.del_row(9)
-            program_plant_name_list.remove(program_plant_name_list[9])
-            print(your_table)
-            print(instructions)
-            update_plant_collection()  
-
-        elif remove_plant == "11":
-            print("\n You removed PLANT 11 from your collection. \n")
-            your_table.del_row(10)
-            program_plant_name_list.remove(program_plant_name_list[10])
-            print(your_table)
-            print(instructions)
-            update_plant_collection()  
-
-        elif remove_plant == "12":
-            print("\n You removed PLANT 12 from your collection. \n")
-            your_table.del_row(11)
-            program_plant_name_list.remove(program_plant_name_list[11])
-            print(your_table)
-            print(instructions)
-            update_plant_collection()  
-
-        elif remove_plant == "13":
-            print("\n You removed PLANT 13 from your collection. \n")
-            your_table.del_row(12)
-            program_plant_name_list.remove(program_plant_name_list[12])
-            print(your_table)
-            print(instructions)
-            update_plant_collection()  
-
-        elif remove_plant == "14":
-            print("\n You removed PLANT 14 from your collection. \n")
-            your_table.del_row(13)
-            program_plant_name_list.remove(program_plant_name_list[13])
-            print(your_table)
-            print(instructions)
-            update_plant_collection()  
-
-        elif remove_plant == "15":
-            print("\n You removed PLANT 15 from your collection. \n")
-            your_table.del_row(14)
-            program_plant_name_list.remove(program_plant_name_list[14])
-            print(your_table)
-            print(instructions)
-            update_plant_collection()  
-
+        if remove_plant <= len(program_plant_name_list):
+                print("\n You removed PLANT in position " + (str(remove_plant)) + " from your collection. \n")
+                your_table.del_row(int(remove_plant))
+                # program_plant_name_list.remove(remove_plant.upper())
+                print(your_table)
+                print(instructions)
+                update_plant_collection()
         else:
-            print("\nIVALID SELECTION! Please select 'A' (ADD Plant), 'R' (Remove Plant) or 'F' (Finalise Collection)\n")
+            print("\nINVALID SELECTION! Please select 'A' (ADD Plant), 'R' (Remove Plant) or 'F' (Finalise Collection)\n")
             update_plant_collection()
-        
+ 
     elif user_option.upper() == "F":
         print("\nThanks, your collection is complete!\n")
          
     else:
         print("\nIVALID SELECTION! Please select 'A' (ADD Plant), 'R' (Remove Plant) or 'F' (Finalise Collection)\n")
         update_plant_collection()
-
 update_plant_collection()
+
+
 
 # Shows user their final plant collection 
 print(your_table)
@@ -443,6 +258,7 @@ add_data_to_lists()
 
 # ZIPS all the plants names from the user with all the plant data from the user together to create a DICT for comparing with plant class data
 all_user_plant_data = dict(zip(program_plant_name_list, user_plant_data_list))
+
 # print(all_user_plant_data)
 print("\n")
 
@@ -458,7 +274,7 @@ print(your_table)
 print("\n\nBased on the details you've submitted, here is some handy information! \n\n")
 
 
-def reccomendations():
+def recommendations():
     for key in all_user_plant_data.keys():
         if key == "MONSTERA":
             PLANTDATA = Plant("MONSTERA", 11, 365, True)
@@ -514,6 +330,9 @@ def reccomendations():
         else:
             print("Your " + key + " is not near a window, this is good!\n")
     print("\n\n")
-reccomendations()
+recommendations()
 
+def terminate_app():
+    print("\n\nThank you for using PlantApp.\n\n")
+terminate_app()
 
